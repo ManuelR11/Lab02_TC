9c21d538070459272d16dd2972881c1cd173a412
jsantis <san21153@uvg.edu.gt>
2023-07-30 21:01:18 -0600

    leer archivo expresiones.txt

--- Ejercicio3/ejercicio3.py
+++ Ejercicio3/ejercicio3.py
@@ -75,20 +75,12 @@ def infix_to_postfix(regex):
 
     return ''.join(postfix)
 
-# Prueba con las expresiones regulares proporcionadas
-expressions = [
-    "(a|t)c",
-    "(a|b)*",
-    "(a*|b*)*",
-    "((E|a)|b*)*",
-    "(a|b)*abb(a|b)*",
-    "0?(1?)?0*",
-    "if\\([ae]+\\)\\{[ei]+\\}(\\n(else\\{[jl]+\\}))?",
-    "[ae03]+@[ae03]+.(com|net|org)(.(gt|cr|co))?"
-]
+with open('./Ejercicio3/expresiones.txt', 'r') as file:
+    expressions = file.readlines()
 
+# Procesar las expresiones y mostrar los resultados en formato postfix
 for expr in expressions:
-    postfix_expression = infix_to_postfix(expr)
-    print(f"Expresión original: {expr}")
+    postfix_expression = infix_to_postfix(expr.strip())
+    print(f"Expresión original: {expr.strip()}")
     print(f"Expresión en formato postfix: {postfix_expression}")
     print()
