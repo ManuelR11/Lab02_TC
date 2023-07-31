bc7afbc32c5b1aa9278cfc04d1b9c6cb1388166a
jsantis <san21153@uvg.edu.gt>
2023-07-30 18:41:47 -0600

    correccion de errores

--- Ejercicio3/ejercicio3.py
+++ Ejercicio3/ejercicio3.py
@@ -1,3 +1,6 @@
+def is_operator(c):
+    return c in ['|', '.', '*', '+', '?']
+
 def get_precedence(c):
     precedence = {
         '(': 1,
@@ -10,57 +13,33 @@ def get_precedence(c):
     }
     return precedence.get(c, 0)
 
-def format_regex(regex):
-    res = ''
-    for i in range(len(regex)):
-        c1 = regex[i]
-
-        if i + 1 < len(regex):
-            c2 = regex[i + 1]
-
-            res += c1
-
-            if (c1 != '(' and c2 != ')' and
-                    (c2 != '?' and c2 != '*' and c2 != '+') and
-                    (c1 != '?' and c1 != '*' and c1 != '+')):
-                res += '.'
-
-    res += regex[-1]
-    return res
-
 def infix_to_postfix(regex):
-    postfix = ''
+    postfix = []
     stack = []
-    formatted_regex = format_regex(regex)
 
-    for c in formatted_regex:
+    for c in regex:
         if c == '(':
             stack.append(c)
 
         elif c == ')':
             while stack and stack[-1] != '(':
-                postfix += stack.pop()
+                postfix.append(stack.pop())
+            stack.pop()  # Eliminar el '(' de la pila
 
-            if stack and stack[-1] == '(':
-                stack.pop()
-            else:
-                postfix += '?'  # Agregar carácter de apertura para corregir desbalance
-
-        else:
+        elif is_operator(c):
             while stack and get_precedence(stack[-1]) >= get_precedence(c):
-                postfix += stack.pop()
+                postfix.append(stack.pop())
             stack.append(c)
 
-    while stack:
-        if stack[-1] == '(':
-            postfix += '+'  # Agregar carácter de cierre para corregir desbalance
-            stack.pop()
         else:
-            postfix += stack.pop()
+            postfix.append(c)
 
-    return postfix
+    while stack:
+        postfix.append(stack.pop())
 
-# Test con las expresiones regulares proporcionadas
+    return ''.join(postfix)
+
+# Prueba con las expresiones regulares proporcionadas
 expressions = [
     "(a|t)c",
     "(a|b)*",
